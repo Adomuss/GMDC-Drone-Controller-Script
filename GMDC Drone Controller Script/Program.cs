407c3d8dac3b2a67eeff05150debc76eb847baad
Adomuss <adomusmusic@gmail.com>
2026-08-26 14:57:28 +0100

    yet another update to screen, second drone line

--- GMDC Drone Controller Script/Program.cs
+++ GMDC Drone Controller Script/Program.cs
@@ -4145,7 +4145,7 @@ namespace IngameScript
             s = droneInformation.Length;
             droneInformation.Append(droneID[ivl]).Append(" Mining: ").Append(drone1.IsMining).Append(" HGR: ").Append(drone1.canlaunch);
             droneInformation.Append(' ', Math.Max(clbs - (droneInformation.Length - s), 0));
-            if (slu) droneInformation.Append(droneID[ivl2]).Append(" Mining: ").Append(drone2.IsMining).Append(" Cl: ").Append(drone2.canlaunch);
+            if (slu) droneInformation.Append(droneID[ivl2]).Append(" Mining: ").Append(drone2.IsMining).Append("  HGR: ").Append(drone2.canlaunch);
             droneInformation.AppendLine();
 
             // Row 5
